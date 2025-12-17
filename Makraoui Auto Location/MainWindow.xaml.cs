6c04ec5d9cccea2eeb22a73b29c1e1b158090dc4
A7meed <102392223+M5INZA@users.noreply.github.com>
2025-12-17 01:10:16 +0100

    Tag1
    Heute habe ich viele Ideen aufgeschrieben und gezeichnet. und habe ich die ersten Steps gemacht. (interface)

---
+++ Makraoui Auto Location/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Makraoui_Auto_Location.View;
+
+namespace Makraoui_Auto_Location
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainContent.Content = new CarsView();
+        }
+    }
+}
+namespace Makraoui_Auto_Location.Models
+{
+    public class Vehicle
+    {
+        public string Plate { get; set; }
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public string Price { get; set; }
+    }
+}
+
